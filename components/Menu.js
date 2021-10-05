c78d8058fb1cb17209d76faf2a32e605d15f4f80
Jenna Blank <jennablank21@gmail.com>
2021-10-05 11:56:44 -0400

    dataset updated

--- components/Menu.js
+++ components/Menu.js
@@ -10,20 +10,36 @@ const DATA = sushiMenu.map((x) => {
   return(x)
 })
 
-const Item = ({title, description, category}) => (
+
+const Item = ({category, items}) => (
   <View style={styles.item}>
-    <Text style={styles.sushiTitle}>{title}</Text>
-    <Text style={{color: "gray"}}>{description}</Text>
-    <Text style={{color: "gray"}}>Category: {category}</Text>
+    <Text style={styles.category}>Category: {category}</Text>
+    { items.map((x) => {
+      console.log(x)
+      if (x.description != "temp") {
+        return (
+          <View style={styles.sushiRoll}>
+            <Text style={{fontWeight: "bold"}}>{x.title}</Text>
+            <Text>{x.description}</Text>
+          </View>
+        )
+      } else {
+        return (
+          <View style={styles.sushiRoll}>
+            <Text style={{fontWeight: "bold"}}>{x.title}</Text>
+          </View>
+        )
+      }
+    }) }
   </View>
 );
 
+
 const Menu = ({navigation}) => {
-  const renderItem = ({item}) => (
+  const renderCategory = ({item}) => (
     <View>
-      <Item title = {item.title}
-            description = {item.description}
-            category = {item.category} />
+      <Item category = {item.category}
+            items = {item.items} />
     </View>
   );
 
@@ -36,12 +52,46 @@ const Menu = ({navigation}) => {
       />
       <FlatList
         data={DATA}
-        renderItem={renderItem}
+        renderItem={renderCategory}
         keyExtractor={item => item.id}
       />
     </SafeAreaView>
   );
 }
+
+// THIS IS THE FLAT DATA VERSION OF THE COMPONENT
+// const Item = ({title, description, category}) => (
+//   <View style={styles.item}>
+//     <Text style={styles.sushiTitle}>{title}</Text>
+//     <Text style={{color: "gray"}}>{description}</Text>
+//     <Text style={{color: "gray"}}>Category: {category}</Text>
+//   </View>
+// );
+//
+// const Menu = ({navigation}) => {
+//   const renderItem = ({item}) => (
+//     <View>
+//       <Item title = {item.title}
+//             description = {item.description}
+//             category = {item.category} />
+//     </View>
+//   );
+//
+//   return (
+//     <SafeAreaView style={styles.container}>
+//       <Text style={styles.title}>Sushi Menu</Text>
+//       <TextInput
+//         style={styles.input}
+//         placeholder="Search for a roll"
+//       />
+//       <FlatList
+//         data={DATA}
+//         renderItem={renderItem}
+//         keyExtractor={item => item.id}
+//       />
+//     </SafeAreaView>
+//   );
+// }
 
 
 const styles = StyleSheet.create({
@@ -51,7 +101,7 @@ const styles = StyleSheet.create({
   },
   item: {
     flex:4,
-    backgroundColor: '#f9c2ff',
+    //backgroundColor: '#f9c2ff',
     padding: 20,
     marginVertical: 8,
     marginHorizontal: 16,
@@ -59,17 +109,25 @@ const styles = StyleSheet.create({
   sushiTitle: {
     fontSize: 32,
   },
-  title: { 
+  title: {
     fontSize: 32,
     fontWeight: "bold",
     textAlign: "center",
     padding: 15,
   },
-  input: { 
+  input: {
     height: 40,
     margin: 12,
     borderWidth: 1,
     padding: 10,
+  },
+  category: {
+    fontWeight: "bold",
+    fontSize: 25
+  },
+  sushiRoll: {
+    backgroundColor: '#f9c2ff',
+    paddingTop: 10,
   }
 });
 
