1f2ce9460ed04168f9f3a1164591abfc7f09c34a
Jenna Blank <jennablank21@gmail.com>
2021-11-02 10:39:32 -0400

    added screentemplate and footer

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import { Text, } from 'react-native';
+import { Text } from 'react-native';
 
 import Home from './components/Home';
 import About from './components/About';
@@ -10,6 +9,7 @@ import Order from './components/Order';
 import Kitchen from './components/Kitchen';
 import Cart from './components/Cart';
 import ValueProvider from './components/ValueContext'
+import ScreenTemplate from './containers/ScreenTemplate';
 
 const Stack = createNativeStackNavigator();
 
@@ -18,17 +18,19 @@ const App = () => {
 
   return (
     <ValueProvider value = {data}>
-      <NavigationContainer>
-        <Stack.Navigator>
+      <ScreenTemplate footer={<Text  style={{fontSize:32}}>this is the footer </Text>} >
+        <NavigationContainer>
+          <Stack.Navigator>
 
-          <Stack.Screen name="Home" component={Home} />
-          <Stack.Screen name="About" component={About} />
-          <Stack.Screen name="Order" component={Order} />
-          <Stack.Screen name="Cart" component={Cart} />
-          <Stack.Screen name="Kitchen" component={Kitchen} />
+            <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen name="About" component={About} />
+            <Stack.Screen name="Order" component={Order} />
+            <Stack.Screen name="Cart" component={Cart} />
+            <Stack.Screen name="Kitchen" component={Kitchen} />
 
-        </Stack.Navigator>
-      </NavigationContainer>
+          </Stack.Navigator>
+        </NavigationContainer>
+      </ScreenTemplate>
     </ValueProvider>
   );
 }
